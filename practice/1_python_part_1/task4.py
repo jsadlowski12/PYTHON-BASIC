a237b917cc6c2910b63c0de2df395c8ef58386fd
jsadlowski <jsadlowski@griddynamics.com>
2025-06-16 09:21:40 +0200

    task4.py completed.

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -10,4 +10,18 @@ from typing import List
 
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    previous_value = 0
+    previous_value_power = 0
+    new_value = 0
+
+    for i in ints:
+        if i == 0:
+            previous_value = ints[i]
+            previous_value_power = previous_value**2
+            ints[i] = previous_value_power
+            continue
+
+        new_value = ints[i]**2 - (previous_value_power - previous_value)
+        previous_value = ints[i]
+        previous_value_power = previous_value**2
+        ints[i] = new_value
